d3742db5881f9d38828f3967acf5c2dc1b655724
Rob Kenis <rob.kenis@hotmail.com>
2020-09-29 20:27:39 +0200

    Open lastet video in theoplayer demo zone
    TODO: Make the popup a little prettier. Or not, chances of DPG fixing DRM are pretty big.

--- plugins/chrome/popup.js
+++ plugins/chrome/popup.js
@@ -1,7 +1,8 @@
 let openVideo = document.getElementById('openVideo');
+const VIDEO_PLAYER_URL = 'https://demo.theoplayer.com/test-your-stream-with-statistics';
 
 openVideo.onclick = function(element) {
     chrome.storage.local.get('lastVideo', function(data) {
-        window.open(data['lastVideo'], '_blank');
+        window.open(`${VIDEO_PLAYER_URL}?url=${data['lastVideo']}`, '_blank');
     });
 };
